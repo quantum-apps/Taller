80c05c3b2ecb21d9554aa341c5fd4ba27fd3a976
quantum-apps <102760289+quantum-apps@users.noreply.github.com>
2022-04-01 15:08:16 -0700

    d

--- MyApp.py
+++ MyApp.py
@@ -1,7 +1,14 @@
 import streamlit as st
 st.title("My primer App")
-click=st.button("dale click")
-st.write("el valor de click es: ",click)
+
+
+
+with st.sidebar:
+    num1 = st.slider('Elige el numero 1', 0.0, 100.0, 25.0)
+    num2 = st.slider('Elige el numero 2', 0.0, 100.0, 25.0)
+    click=st.button("dale click")
+    st.write("el valor de click es: ",click)
+
 
 if click==True:
     st.image("perro.jpg")
@@ -11,8 +18,7 @@ if click==True:
 #st.latex(" \int_1^6 sin(x)dx")
 #st.markdown(" #titulo ")
 #st.markdown(" **este es una vivnieta** ")
-num1 = st.slider('Elige el numero 1', 0.0, 100.0, 25.0)
-num2 = st.slider('Elige el numero 2', 0.0, 100.0, 25.0)
+
 suma = num1+num2
 st.write("la suma de",num1," y ",num2,"es :",suma)
 
