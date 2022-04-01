57a72283d5a54920fea020d64d9793f733e054c4
quantum-apps <102760289+quantum-apps@users.noreply.github.com>
2022-04-01 14:59:17 -0700

    d

--- MyApp.py
+++ MyApp.py
@@ -11,3 +11,7 @@ if click==True:
 #st.latex(" \int_1^6 sin(x)dx")
 #st.markdown(" #titulo ")
 #st.markdown(" **este es una vivnieta** ")
+num1 = st.slider('Elige el numero 1', 0.0, 100.0, 25.0)
+num2 = st.slider('Elige el numero 2', 0.0, 100.0, 25.0)
+suma = num1+num2
+st.write("la suma de",num1," y ",num2,"es :",suma)
