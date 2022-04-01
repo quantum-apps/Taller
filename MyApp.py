ab370757cdc66d3758c049d3b91f81d4ff2233d1
quantum-apps <102760289+quantum-apps@users.noreply.github.com>
2022-04-01 14:53:05 -0700

    d

--- MyApp.py
+++ MyApp.py
@@ -2,6 +2,9 @@ import streamlit as st
 st.title("My primer App")
 click=st.button("dale click")
 st.write("el valor de click es: ",click)
+
+if click==True:
+    st.image("perro.jpg")
 #st.button("otro boton")
 #st.text("Hola mundo")
 #st.text("la siguiente es una integral")
